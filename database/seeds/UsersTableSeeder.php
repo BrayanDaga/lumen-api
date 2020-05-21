b590ee0499ced4c524eed947b54bbdc3bd877a8c
Brayan_Vilchez_Daga <brayandaga5@gmail.com>
2020-05-20 22:53:05 -0500

    first commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\User::create([
+            'name'      =>   'Brayan Vilchez Daga',
+            'username'  =>    'BrayanDaga',
+            'email'     =>   'bm_vd_1605@hotmail.com',
+            'password'  =>   Hash::make('brayanvilchez'),
+            'apitoken' => Str::random(60)
+        ]);
+    }
+}
